fe3aa8155cfa3eef360eb11ca58e502e9f220acb
Choi Hyun seo <chss0520@sookmyung.ac.kr>
2023-06-17 05:07:40 +0900

    백엔드 개발 18. @Query 사용하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -11,13 +11,13 @@ public class TodoService {
 	private TodoRepository repository;
 	public String testService(){
 	// Todo Entity 생성
+	TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
 	
-		TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
-	//Todo Entity 저장
+	// Todo Entity 저장
+	repository.save(entity);
 	
-		repository.save(entity);
-	//Todo Entity 검색
+	// Todo Entity 검색
 	TodoEntity savedEntity = repository.searchByUserId(entity.getUserId()).get(0);
-	return savedEntity.getUserId();
+		return savedEntity.getUserId();
 	}
 }
