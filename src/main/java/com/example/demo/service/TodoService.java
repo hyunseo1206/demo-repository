faa41990a5722248eb15ffe7c1a499ce28c2c9b5
Choi Hyun seo <chss0520@sookmyung.ac.kr>
2023-06-17 05:05:44 +0900

    백엔드 개발 18. @NamedQuery 사용하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -9,11 +9,15 @@ import com.example.demo.persistence.TodoRepository;
 public class TodoService {
 	@Autowired
 	private TodoRepository repository;
+	public String testService(){
+	// Todo Entity 생성
 	
-	public String testService() {
 		TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
+	//Todo Entity 저장
+	
 		repository.save(entity);
-		TodoEntity savedEntity = repository.findByUserId(entity.getUserId()).get(0);
-		return savedEntity.getUserId();
+	//Todo Entity 검색
+	TodoEntity savedEntity = repository.searchByUserId(entity.getUserId()).get(0);
+	return savedEntity.getUserId();
 	}
 }
